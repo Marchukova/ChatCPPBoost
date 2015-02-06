431ced4c832ef7a842257118f43603853e09e074
Tatyana Marchukova <tmarchukova@gmail.com>
2015-02-06 18:20:20 +0300

    Send messages to other clients

--- server.cpp
+++ server.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <boost/thread.hpp>
 #include <boost/date_time.hpp>
+#include <boost/bind.hpp>
 
 #include "server.h"
 
@@ -12,25 +12,49 @@ Server::Server(const std::string &address, unsigned short port)
 	: ep(ip::address::from_string(address), port), acc(service, ep) {
 }
 
-/* Read data from client and write to standart output. 
+/* If a pointer is unique, then thread for the pointer is cancelled.
+/* The function for deletion.
+*/
+bool lastPtr(boost::shared_ptr<ip::tcp::socket> & element, ip::tcp::socket *socket) {
+	if (element.get() == socket)
+		return false;
+	return element.unique();
+} /* End of 'lastPtr' function */
+
+/* Read data from client and send it to others client. 
 /* Thread function.
 */
-void Server::readWrite(boost::shared_ptr<ip::tcp::socket> sock) {
+void Server::readWrite(boost::shared_ptr<ip::tcp::socket> & sockPtr) {
 	try {
 		boost::asio::streambuf buf;
-		//ostream out(&buf); //stream to write to the buffer
-		while (read_until(*sock, buf, "\n")) { 	// read data from client 
+		while (read_until(*sockPtr, buf, "\n")) { 	// read data from client 
+			clientsMutex.lock();
+				/* Delete all client who disconnect(but not current) */
+				clients.remove_if(boost::bind(lastPtr, _1, sockPtr.get()));
+				/* Send message to others */
+				for (list<boost::shared_ptr<ip::tcp::socket> >::iterator it = clients.begin();
+						it != clients.end(); ++it) 
+					if (it->get() != sockPtr.get()) { //send if not current socket
+						write(**it, buf.data());
+					}		
+			clientsMutex.unlock();			
 			cout << &buf; // dump everything in console
 		}
 	} catch (boost::system::system_error) { //connection was closed
 	}
-} /* End of 'readWrite' function */
+} /* End of 'Server::readWrite' function */
 
+/* Connect new client and start a thread for it. 
+*/
 void Server::run() {
 	while (true) {
-		boost::shared_ptr<ip::tcp::socket> sock_ptr(new ip::tcp::socket(service));
-		acc.accept(*sock_ptr); //wait connection
-		boost::thread thread(&Server::readWrite, this, sock_ptr); //Start a thread for new client
+		boost::shared_ptr<ip::tcp::socket> sockPtr(new ip::tcp::socket(service));
+		acc.accept(*sockPtr); //wait for connection
+		
+		clientsMutex.lock();
+			clients.push_back(sockPtr);
+			boost::thread thread(&Server::readWrite, this, sockPtr); //Start a thread for a new client
+		clientsMutex.unlock();
 	}
-}
+} /* End of 'Server::run' function */
 
--- server.h
+++ server.h
@@ -3,7 +3,9 @@
 #define _SERVER_H_
 #define _WIN32_WINNT 0x0501
 
+#include <list>
 #include <boost/asio.hpp>
+#include <boost/thread.hpp>
 #include <boost/shared_ptr.hpp>
 
 #define BOOST_ASIO_SEPARATE_COMPILATION
@@ -12,11 +14,18 @@ class Server {
 	boost::asio::io_service service; 
 	boost::asio::ip::tcp::endpoint ep;
 	boost::asio::ip::tcp::acceptor acc; //object for client's connection
-
-	void readWrite(boost::shared_ptr<boost::asio::ip::tcp::socket> sock);
+	
+	/* List of all current server's clients */
+	std::list<boost::shared_ptr<boost::asio::ip::tcp::socket> > clients;
+	/* Mutex to work with list of clients */
+	boost::mutex clientsMutex;
+	
+	/* Read data from client and send it to others client */
+	void readWrite(boost::shared_ptr<boost::asio::ip::tcp::socket> & sockPtr);
 public:
 	Server(const std::string &address, unsigned short port);
-	/* Read data from client and write to standart output */
+	
+	/* Connect new client and start a thread for it */
 	void run();
 };
 
